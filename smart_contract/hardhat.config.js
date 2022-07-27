32e539bfa68ff533e86879433bb69c8857f24bc2
samuelochaba <nerdysam5826@gmail.com>
2022-07-27 09:24:58 +0100

    add transaction smart contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-goerli.g.alchemy.com/v2/n3sj8Whuh5GIDRdGxYZgc36sp1H1szX_
+
+// require("@nomicfoundation/hardhat-toolbox");
+
+require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/n3sj8Whuh5GIDRdGxYZgc36sp1H1szX_",
+      accounts: [
+        "3efb735f018ace13e1378c5aae18446d7dae01140174936a439d1f93dc810c3d",
+      ],
+    },
+  },
 };
